af8cee6dbe1aa523eddf2dc9f23b3f54c0441293
PUSHPAGHJH <nagatolabyap@gmail.com>
2021-12-07 12:56:02 +0530

    uploaded

--- src/App.js
+++ src/App.js
@@ -1,58 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './component/Header';
+import Profilesec from './component/Profilesec';
+import Feed from './component/Feed';
+import Projectsec from './component/Projectsec';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    <>
+    
+<div className='app_header'>
+<Header />
+</div>
+<div className='sidebar'>
+<Profilesec />
+<Feed />
+<Projectsec />
+
+</div>
+</>
+    );
 }
 
 export default App;
